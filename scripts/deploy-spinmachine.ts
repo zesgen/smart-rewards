5190b7bd6037fb222f558787789fc2cd21a1c7bb
Igor Senych <42384549+gitigs@users.noreply.github.com>
2021-02-04 17:31:25 +0200

    Added openzeppelin upgrades.

--- scripts/deploy-spinmachine.ts
+++ scripts/deploy-spinmachine.ts
@@ -1,8 +1,8 @@
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 
-async function main() {    
+async function main() {          
   const SpinMachine = await ethers.getContractFactory("SpinMachineV1");
-  const spinMachine = await SpinMachine.deploy();
+  const spinMachine = await upgrades.deployProxy(SpinMachine);
   await spinMachine.deployed();
   console.log("SpinMachineV1 deployed to:", spinMachine.address);
 }
