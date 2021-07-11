406ef5da693d7854e6828ff60b5c043a38955569
IgorSenychCW <72256997+IgorSenychCW@users.noreply.github.com>
2021-07-11 21:06:56 +0300

    Implement Pausable contract.

---
+++ contracts/utils/PausablePauser.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+
+contract PausablePauser is OwnableUpgradeable, PausableUpgradeable {
+    address private _pauser;
+
+    event PauserChanged(address indexed pauser);
+
+    function __PausablePauser_init() internal initializer {
+        __Context_init_unchained();
+        __Ownable_init_unchained();
+        __Pausable_init_unchained();
+        __PausablePauser_init_unchained();
+    }
+
+    function __PausablePauser_init_unchained() internal initializer {
+    }
+
+    /**
+     * @dev Throws if called by any account other than the pauser.
+     */
+    modifier onlyPauser() {
+        require(_pauser == _msgSender(), "PausablePauser: caller is not the pauser");
+        _;
+    }
+
+    /**
+     * @dev Triggers paused state. Can only be called by the pauser account.
+     * Requirements:
+     * - The contract must not be paused.
+     */
+    function pause() external onlyPauser {
+        _pause();
+    }
+
+    /**
+     * @dev Triggers unpaused state. Can only be called by the pauser account.
+     * Requirements:
+     * - The contract must be paused.
+     */
+    function unpause() external onlyPauser {
+        _unpause();
+    }
+
+    /**
+     * @dev Sets `pauser` address. Can only be called by the contract owner.
+     * Emits an {PauserChanged} event.
+     */
+    function setPauser(address newPauser) external onlyOwner {
+        _pauser = newPauser;
+        emit PauserChanged(_pauser);
+    }
+
+    /**
+     * @dev Returns `pauser` address.
+     */
+    function getPauser() external view returns(address) {
+        return _pauser;
+    }
+}
