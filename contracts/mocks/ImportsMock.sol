a7ae5a1c7333f94f6c77c19c42417f3761c15954
IgorSenychCW <72256997+IgorSenychCW@users.noreply.github.com>
2021-08-25 21:21:34 +0300

    Add ImportsMock.sol contract.

---
+++ contracts/mocks/ImportsMock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/proxy/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
+
+abstract contract ImportsMock {}
