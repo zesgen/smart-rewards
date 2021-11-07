9ef726dd525329e33dac6c430b8b51505c1aa1a6
Evgeniy Zaytsev <zesgen@gmail.com>
2021-11-07 22:33:03 +0700

    Add the 'solidity-coverage' plugin for Hardhat
    More information about the plugin is here: https://github.com/sc-forks/solidity-coverage/blob/master/HARDHAT_README.md

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-etherscan";
 import "@eth-optimism/hardhat-ovm";
+import "solidity-coverage";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
